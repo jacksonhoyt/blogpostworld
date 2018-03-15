a9a0387a32080ce43879c27526adf5b1346973b3
jacksonhoyt <jackson.t.hoyt@gmail.com>
2018-03-14 22:32:44 -0500

    setting up blogpost component

---
+++ src/components/blogpost_index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BlogPostIndex extends Component {
+  render() {
+    return(
+      <div>
+        BLOGPOSTINDEX
+      </div>
+    );
+  }
+}
+
+export default BlogPostIndex;
