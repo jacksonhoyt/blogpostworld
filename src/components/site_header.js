f7acec230da1e960e9637b73c5949c6399f35b34
jacksonhoyt <jackson.t.hoyt@gmail.com>
2018-07-04 23:00:16 -0500

    Saving work on blogsite project

---
+++ src/components/site_header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { ButtonToolbar } from 'react-bootstrap';
+
+
+class SiteHeader extends Component {
+  render() {
+    return (
+      <div>
+        <h1>blOGpoSTwoRLD!!!</h1>
+        <h6>--benefit others by empowering mothers--</h6>
+        <hr />
+      </div>
+    )
+  }
+
+}
+
+export default SiteHeader;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+import BlogPostReducer from './reducer_blogposts';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  posts: BlogPostReducer,
+  form: formReducer
 });
 
 export default rootReducer;
